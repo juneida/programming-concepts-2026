a1ed0097d3c941fcab1c1ac27e329e38398ae757
juneida <juneida20@hotmail.com>
2026-06-09 10:55:36 +0400

    Add files via upload

---
+++ BMI.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+
+{
+
+
+float h,w,BMI;
+message:
+printf("Enter height in meter:");
+scanf("%f",&h);
+
+	
+
+if(h<0)
+{
+
+	printf("\n Positive values only");
+goto message;
+}
+	
+printf("\nEnter weight in kg:");
+scanf("%f",&w);
+
+
+BMI = w/(h*h); 
+
+printf("\n\nYour Body Mass Index (BMI) is %.1f",BMI);
+
+
+
+}
+
+
